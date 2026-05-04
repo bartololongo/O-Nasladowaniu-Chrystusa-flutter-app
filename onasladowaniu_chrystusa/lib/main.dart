36c3e63caebd7e81eaad22437dd1a83ec87878b1
Bartolo Longo <bartolo-longo@MacBook-Pro-Bartolo.local>
2026-05-04 21:11:50 +0200

    Lock app orientation to portrait

--- onasladowaniu_chrystusa/lib/main.dart
+++ onasladowaniu_chrystusa/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 
 import 'app.dart';
@@ -7,6 +8,7 @@ import 'shared/services/formation_widget_snapshot_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await JustAudioBackground.init(
     androidNotificationChannelId: 'pl.bartololongo.onasladowaniu.audio',
     androidNotificationChannelName: 'O naśladowaniu Chrystusa',
